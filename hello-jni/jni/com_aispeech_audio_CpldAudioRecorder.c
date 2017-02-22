d7a60d0888bcfa076089d979de8a5f989993e7c9
ruilong.yu <ruilong.yu@aispeech.com>
2017-02-22 11:20:56 +0800

    update

--- hello-jni/jni/com_aispeech_audio_CpldAudioRecorder.c
+++ hello-jni/jni/com_aispeech_audio_CpldAudioRecorder.c
@@ -51,14 +51,22 @@
 #define LOG_TAG "CPLD_AUDIO_JNI"
 #define LOG_NDEBUG 1
 #define PCM_IN     0x10000000
-#define CPLD_AUDIO_CHANNEL_NUM   8
-#define  CHANNELBITS_16   0
-#define  CHANNELBITS_24   1
+#define CPLD_AUDIO_CHANNEL_NUM      2
+#define CPLD_AUDIO_RATE             64000
+#define CPLD_AUDIO_PERIOD_SIZE      8000
+#define CPLD_AUDIO_PERIOD_COUNT     8
+
+#define CPLD_REAL_CHANNELS          8
+#define CPLD_REAL_FRAME_SIZE        (sizeof(unsigned short) * CPLD_REAL_CHANNELS)
+
+static unsigned char g_s24le_data[CPLD_AUDIO_PERIOD_SIZE * CPLD_AUDIO_CHANNEL_NUM * 4]; //0.125 sec data
+static unsigned int  g_s24le_data_pos = sizeof(g_s24le_data);
+static unsigned int  g_s24le_periods = 0;
+
 static struct pcm *g_pcm = NULL;
 
-FILE *file;
-FILE *raw_file;
-int save_audio = 0;
+static FILE *g_s16le_pcm_file;
+static int g_save_audio = 0;
 
 #define ALOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__) // 定义LOGD类型
 #define ALOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__) // 定义LOGI类型
@@ -136,58 +144,74 @@ static void tinymix_set_value(struct mixer *mixer, const char *control,
  * Method:    native_read
  * Signature: ([BII)I
  */
-#define u32 unsigned int
-char remaind[32];
-int  rmaind_cnt;
-int find_channel0(u32 *RXBuff,u32 RXBuffLen)
-{
-    u32 Channel1Pos;
-    u32 *p;
-    p=RXBuff;
-    while(!(*p&0x100))
-    {
-        if(p < RXBuff + RXBuffLen - 1)
-        p++;
-        else return -1;
-    }
-    return (p - RXBuff);
-}
-
-int check_channel0(char RXBuff)
-{
-    if(RXBuff & 0x01) 
-    {
-        return 1;
-    } 
-    else 
-    {
-        return 0;
-    } 
-}
 
 static void JNICALL Java_com_aispeech_audio_CpldAudioRecorder_native_1save_audio(JNIEnv *env, jclass clazz) {
-    save_audio = 1;
+    g_save_audio = 1;
 }
 
+static int cpld_s16le_pcm_read(struct pcm *pcm, char *buffer, int size)
+{
+    unsigned int s24le_value, *p_s24le_value;
+    unsigned short *p_s16le_dst_buf;
+    int dst_buf_index, frames;
 
+    if (size % CPLD_REAL_FRAME_SIZE) {
+        ALOGE("Incorrect pcm read size (%d), should be N*%d", size, CPLD_REAL_FRAME_SIZE);
+        return -1;
+    }
+
+    frames = 0;
+    p_s16le_dst_buf = (unsigned short *)buffer;
+    for (dst_buf_index=0; dst_buf_index<(size / sizeof(unsigned short)); dst_buf_index++) {
+        if (g_s24le_data_pos >= sizeof(g_s24le_data)) {
+            g_s24le_data_pos = 0;
+            if (!pcm_read(pcm, &g_s24le_data[0], sizeof(g_s24le_data))) {
+                g_s24le_periods++;
+            } else {
+                ALOGE("pcm read error, pcm handler=0x%08x", pcm);
+                return -1;
+            }
+        }
+
+        p_s24le_value = (unsigned int *)&g_s24le_data[g_s24le_data_pos];
+        s24le_value = (*p_s24le_value) >> 8;
+        if (s24le_value & 0x01) {
+            // Firt channel
+            if (dst_buf_index % CPLD_REAL_CHANNELS) {
+                ALOGW("WARNING: s24le to s16le is not aligned, dst_buf_index=%d, frames=%d, g_s24le_periods=%u\n", dst_buf_index, frames, g_s24le_periods);
+                dst_buf_index = (dst_buf_index / CPLD_REAL_CHANNELS) * CPLD_REAL_CHANNELS;
+            }
+
+            frames++;
+            if (frames > (size / CPLD_REAL_FRAME_SIZE * 2)) {
+                //Invalid data, assume read frames is impossible over 2 expected frames.
+                frames = 0;
+                break;
+            }
+        }
+
+        *(p_s16le_dst_buf + dst_buf_index) = (unsigned short)(s24le_value >> 8);
+        g_s24le_data_pos = g_s24le_data_pos + sizeof(unsigned int);
+    }
+
+    if (frames) {
+        return size;
+    } else {
+        return -1;
+    }
+}
 
 static jint JNICALL Java_com_aispeech_audio_CpldAudioRecorder_native_1read(JNIEnv *env, jclass clazz, jbyteArray javaAudioData,
     jint channelBits, jint sizeInBytes)
 {
     jbyte* recordBuff = NULL;
-    jbyte* p =NULL ;
-    int sendPointCnt;
-    int needReadBytes;
-    int ch1_pos;
-    if (!g_pcm)
-    {  
+    int readbytes;
+
+    if (!g_pcm) {  
        ALOGE("g_pcm is null");
        return 0;
     }
-    if (!pcm_is_ready(g_pcm))
-    {
-        // fprintf(stderr, "Unable to open PCM device (%s)\n",
-        // pcm_get_error(g_pcm));
+    if (!pcm_is_ready(g_pcm)) {
         ALOGE("Unable to open PCM device (%s)\n", pcm_get_error(g_pcm));
         return 0;
     }
@@ -197,174 +221,26 @@ static jint JNICALL Java_com_aispeech_audio_CpldAudioRecorder_native_1read(JNIEn
     }
 
     recordBuff = (jbyte *)(*env)->GetByteArrayElements(env, javaAudioData, NULL);
-  
-    if (recordBuff == NULL) 
-    {
+    if (recordBuff == NULL)  {
         ALOGE("Error retrieving destination for recorded audio data, can't record");
         return 0;
     }
 
-
-    jbyte* recordBuff_read=recordBuff+32;
-    u32 cnt_read=sizeInBytes-32;
-    u32 channels_NUM =8;//[0,7]
-
-    if (pcm_read(g_pcm,recordBuff_read ,cnt_read ) < 0) //==0 is OK 
-    {
+    readbytes = cpld_s16le_pcm_read(g_pcm, recordBuff, sizeInBytes);
+    if (readbytes > 0) {
+    } else {
+        readbytes = 0;
         ALOGE("Invalid operation, handler=0x%08x", g_pcm);
-        return 0;
     }
 
-    if(save_audio)
-    {  
-        if (fwrite(recordBuff_read, 1, cnt_read, raw_file) != cnt_read)
-        {
+    if ((g_s16le_pcm_file) && (readbytes > 0)) {
+        if (fwrite(recordBuff, 1, readbytes, g_s16le_pcm_file) != readbytes) {
             ALOGE("Error capturing raw_file\n");
         }
     }
 
-    ch1_pos=find_channel0((u32 *)recordBuff_read,8);
-    if(ch1_pos<0)
-    {
-        ALOGE("find_channel0 Error head");
-        //return 0;
-        p=recordBuff_read-(rmaind_cnt)*4;
-
-        memcpy(p, remaind, rmaind_cnt*4);
-        sendPointCnt =cnt_read/4+rmaind_cnt;
-    }
-    else if((ch1_pos+rmaind_cnt) % (channels_NUM)==0)
-    {
-       p=recordBuff_read-(rmaind_cnt)*4;
-
-       memcpy(p, remaind, rmaind_cnt*4);
-       //ALOGE(" sizeInBytes= %d", sizeInBytes);
-       sendPointCnt =cnt_read/4+rmaind_cnt;
-       //ALOGE(" sendPointCnt =  %d", sendPointCnt);
-    }
-    else 
-    {
-        p =recordBuff_read+ch1_pos*4;
-        sendPointCnt =cnt_read/4 -ch1_pos;
-        //ALOGE(" sendPointCnt=%d", sendPointCnt);
-
-    }
-
-
-    ch1_pos=find_channel0((u32 *)((recordBuff_read+cnt_read-32)),8);
-    //if(ch1_pos<0)
-    //{
-        //ALOGE("find_channel0 Error tail");
-        //return 0;
-    //}
-         //ALOGE("SEC_ch1_pos %d", ch1_pos);
-    if(ch1_pos > 0) 
-    {
-        rmaind_cnt =8 -ch1_pos;
-        memcpy(remaind, recordBuff_read+cnt_read-(rmaind_cnt)*4 , rmaind_cnt*4);
-        sendPointCnt-=rmaind_cnt;
-    }
-    else
-    {
-        rmaind_cnt =0;
-    } 
-          //  3 copy mass data
-
-          if(channelBits&0x1)
-          {
-                int i=0;
-                int destIndex=0;
-                int lastIndex=0;
-                while(i<sendPointCnt)
-                {
-                    int isChannel0 = check_channel0(*(p + i*4 + 1));
-                    if(destIndex % 8 == 0 && !isChannel0)
-                    {
-                        i++;
-                        continue;
-                    }
-                    else if(destIndex % 8 != 0 && isChannel0) 
-                    {
-                        //let destIndex to be last channel0 index
-                        destIndex = lastIndex;
-                        i++;
-                        continue;
-                    }
-                    else if(destIndex % 8 ==0 && isChannel0)
-                    {
-                        lastIndex = destIndex;
-                    }
-                    
-                    recordBuff[destIndex*3]  = *(p+i*4+1);    
-                    recordBuff[destIndex*3+1]= *(p+i*4+2);   
-                    recordBuff[destIndex*3+2]= *(p+i*4+3);
-                    
-                    destIndex++;
-                    i++;
-
-                }
-                (*env)->ReleaseByteArrayElements(env, javaAudioData, recordBuff, 0);
-                if(destIndex < 8)
-                { 
-                  rmaind_cnt = 0;
-                  return (jint) 0;
-                }
-                return (jint) destIndex*3;
-
-          }else //CHANNELBITS_16
-          {
-              int i=0;
-              int destIndex=0;
-              int lastIndex=0;
-              while(i<sendPointCnt)
-              {
-                int isChannel0 = check_channel0(*(p + i*4 + 1));
-                if(destIndex % 8 == 0 && !isChannel0)
-                {
-                    ALOGE("destIndex : %d, i : %d, expected 0 channel, but not 0 channel", destIndex, i);
-                    i++;
-                    continue;
-                }
-                else if(destIndex % 8 != 0 && isChannel0) 
-                {
-                    ALOGE("destIndex : %d, i : %d, expected not 0 channel, but 0 channel", destIndex, i);
-                    //let destIndex to be last channel0 index
-                    destIndex = lastIndex;
-                    i++;
-                    continue;
-                }
-                else if(destIndex % 8 ==0 && isChannel0)
-                {
-                    lastIndex = destIndex;
-                }
-                recordBuff[destIndex*2]  =  *(p+i*4+2); 
-                recordBuff[destIndex*2+1]= *(p+i*4+3);
-
-                destIndex++;
-                i++;
-              }
-              if(destIndex < 8)
-              { 
-                 rmaind_cnt = 0;
-                 (*env)->ReleaseByteArrayElements(env, javaAudioData, recordBuff, 0);
-                 return (jint) 0;
-              }                
-
-              if(save_audio)
-              {  
-                  if (fwrite(recordBuff, 1, sendPointCnt*2, file) != sendPointCnt*2)
-                  {
-                    ALOGE("Error capturing sample\n");
-                  }
-              }
-        
-                (*env)->ReleaseByteArrayElements(env, javaAudioData, recordBuff, 0);
-                // ALOGE("sendPointCnt*2= %d ", sendPointCnt*2);//32000
-                return (jint) destIndex*2;
-
-          }
-          
-        return 0;
+    (*env)->ReleaseByteArrayElements(env, javaAudioData, recordBuff, 0);
+    return (jint)readbytes;
 }
 
 
@@ -380,10 +256,10 @@ static jint JNICALL Java_com_aispeech_audio_CpldAudioRecorder_native_1setup(JNIE
     char *buffer;
     unsigned int size;
     unsigned int bytes_read = 0;
-    config.channels = 2;
-    config.rate = 64000;
-    config.period_size = 8000;
-    config.period_count = 8;
+    config.channels = CPLD_AUDIO_CHANNEL_NUM;
+    config.rate = CPLD_AUDIO_RATE;
+    config.period_size = CPLD_AUDIO_PERIOD_SIZE;
+    config.period_count = CPLD_AUDIO_PERIOD_COUNT;
     config.format = PCM_FORMAT_S24_LE;
     config.silence_threshold = 0;
     config.start_threshold = 0;
@@ -409,19 +285,15 @@ static jint JNICALL Java_com_aispeech_audio_CpldAudioRecorder_native_1setup(JNIE
         return 0;
     }
 
+    g_s24le_data_pos = sizeof(g_s24le_data);
+    g_s24le_periods = 0;
 
-    if(save_audio)
+    if(g_save_audio)
     {    
-        if(file !=NULL)
-            fclose(file);
-        file = fopen("/sdcard/raw16bit.pcm", "wb");
-        if (!file) 
-            ALOGE("Unable to create raw16bit.pcm file ");
-        if(raw_file != NULL)
-            fclose(raw_file);
-        raw_file = fopen("/sdcard/raw32bit.pcm", "wb");
-        if(raw_file != NULL)
-            ALOGE("Unable to create raw32bit.pcm file ");
+        if (g_s16le_pcm_file !=NULL) fclose(g_s16le_pcm_file);
+
+        g_s16le_pcm_file = fopen("/sdcard/raw16bit.pcm", "wb");
+        if (!g_s16le_pcm_file) ALOGE("Unable to create /sdcard/raw16bit.pcm file ");
     }
 
     return 0;
@@ -436,10 +308,8 @@ static void JNICALL Java_com_aispeech_audio_CpldAudioRecorder_native_1release(JN
     if (g_pcm) {
         pcm_close(g_pcm);
         g_pcm = NULL;
-        if(file != NULL)
-        fclose(file);
-        if(raw_file != NULL)
-        fclose(raw_file);
+        if(g_s16le_pcm_file != NULL)
+        fclose(g_s16le_pcm_file);
     }
 }
 
